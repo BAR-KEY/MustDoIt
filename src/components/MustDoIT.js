ee0cc2db275be9dbb88557213211aa7d05cfcf4a
BAR-KEY <park96h@naver.com>
2021-11-22 10:34:59 +0900

    MustToDo first

---
+++ src/components/MustDoIT.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./MustDoIt.css";
+
+const MustDoIT = (props) => {
+  return (
+    <div className="MustDoIt">
+      <nav className="nav">
+        <div className="nav_title">MustDoIt</div>
+      </nav>
+    </div>
+  );
+};
+
+export default MustDoIT;
